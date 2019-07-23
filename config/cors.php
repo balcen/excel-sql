228601652c4b6002a3520f5a13ef48532874c2a5
baiceng lu <lbaiceng@gmail.com>
2019-07-23 11:24:17 +0800

    Change cors.php to lpcen.com

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
 
     'supportsCredentials' => false,
-    'allowedOrigins' => ['https://lpcen.com', 'http://172.16.110.7'],
+    'allowedOrigins' => ['https://lpcen.com'],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
