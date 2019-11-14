1b06bc2651f619d018966e8d9ebdbc6254af3021
baiceng lu <lbaiceng@gmail.com>
2019-11-14 18:13:03 +0800

    register function unauthorize

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     public function __construct() {
-        $this->middleware('auth:api', ['except' => ['login', 'refresh']]);
+        $this->middleware('auth:api', ['except' => ['login', 'refresh', 'register']]);
     }
 
     public function register(RegisterFormRequest $request)
