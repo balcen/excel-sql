2124b96d6da7ac24bed8fad8edbc80ab42d038ac
baiceng lu <lbaiceng@gmail.com>
2019-11-25 15:08:37 +0800

    增加 products weight 長度

---
+++ database/migrations/2019_11_25_145551_edit_weight_to_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditWeightToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('p_weight', 100)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
