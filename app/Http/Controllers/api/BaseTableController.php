e37c29d9e36254bab317c1002437d6506f797ef5
baicen <jameslu8044@gmail.com>
2020-04-04 23:43:42 +0800

    init BaseTableController.php

---
+++ app/Http/Controllers/api/BaseTableController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use app\Services\Table\TableService as Service;
+
+class BaseTableController extends Controller
+{
+    protected $service;
+
+    public function __construct(Service $service)
+    {
+        $this->middleware('auth:api');
+        $this->service = $service;
+    }
+
+    public function index(Request $request)
+    {
+        return response()->json($this->service->index($request));
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json($this->service->store($request));
+    }
+
+    public function show(Request $request, $id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+        return response()->json($this->service->update($request, $id));
+    }
+
+    public function destroy($id)
+    {
+        return response()->json($this->service->destroy($id));
+    }
+
+
+    public function batchDelete(Request $request)
+    {
+        return response()->json($this->service->batchDelete($request));
+    }
+
+    public function upload(Request $request)
+    {
+    }
+
+    public function searchAll(Request $request)
+    {
+    }
+
+    private function sortInit()
+    {
+
+    }
+}
