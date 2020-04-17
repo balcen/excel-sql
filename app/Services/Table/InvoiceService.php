2955c31a07f78122723a5dbcab2ec0d3b450aa55
baicen <jameslu8044@gmail.com>
2020-04-17 14:16:27 +0800

    fixed columns type

--- app/Services/Table/InvoiceService.php
+++ app/Services/Table/InvoiceService.php
@@ -7,7 +7,7 @@ use App\Services\ExcelService;
 
 class InvoiceService extends TableService
 {
-    private $columns = ['i_no', 'i_date', 'i_mature', 'i_order_no', 'i_seller_name', 'i_buyer_name', 'i_product_name',
+    protected $columns = ['i_no', 'i_date', 'i_mature', 'i_order_no', 'i_seller_name', 'i_buyer_name', 'i_product_name',
         'i_product_part_no', 'i_product_spec', 'i_product_price', 'i_currency', 'i_quantity', 'i_amount', 'i_note'];
 
     function model()
--- app/Services/Table/OrderService.php
+++ app/Services/Table/OrderService.php
@@ -7,7 +7,7 @@ use App\Services\ExcelService;
 
 class OrderService extends TableService
 {
-    private $columns = ['o_no', 'o_date', 'o_seller_name', 'o_buyer_name', 'o_product_name', 'o_product_part_no',
+    protected $columns = ['o_no', 'o_date', 'o_seller_name', 'o_buyer_name', 'o_product_name', 'o_product_part_no',
         'o_product_spec', 'o_product_price', 'o_currency', 'o_quantity', 'o_amount', 'o_note'];
 
     function model()
